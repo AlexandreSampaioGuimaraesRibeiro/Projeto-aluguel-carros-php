7efbbb431f4940ae1ddf2dbede20b91109e292e5
AlexandreSampaioGuimaraesRibeiro <lexandre.guimaraes@gmail.com>
2026-02-04 20:41:35 -0300

    base
    base do projeto

---
+++ app/Models/carros.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class carros extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['modelo','placa','marca','ano','preco_aluguel','descricao'];
+}
