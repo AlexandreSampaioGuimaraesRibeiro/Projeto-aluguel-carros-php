df39898001a83e3c5cc8d99c546b3c3f565fd09c
AlexandreSampaioGuimaraesRibeiro <lexandre.guimaraes@gmail.com>
2026-02-04 21:23:14 -0300

    Update carrosController.php

--- app/Http/Controllers/carrosController.php
+++ app/Http/Controllers/carrosController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class CarrosController extends Controller
 {
-    //
+    // 
+    public function index()
+    {
+        //indice ordernado por id
+        $carros= Carros::orderByDesc('id')->get();
+        return view('carros.index',compact('carros'));  
+    }
 }
